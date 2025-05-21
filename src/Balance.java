de047332ba121ab371109411905bdfab8c6d02c8
LeoChen128 <leo1281515@gmail.com>
2025-05-21 06:46:03 -0400

    Updated Balance class so that it isn't redundant.

--- src/Balance.java
+++ src/Balance.java
@@ -1,9 +1,8 @@
-
-public class Balance{
+public class Balance {
 
     private int balance;
 
-    public Balance(){
+    public Balance() {
         this.balance = 10000;
     }
 
@@ -11,59 +10,35 @@ public class Balance{
         return balance;
     }
 
-    public void whiteChip(){
-        if (balance < 1){
+    public void deduct(int amount) {
+        if (balance < amount) {
             System.out.println("You don't have enough cash!");
-        }
-        else{
-            balance -= 1;
+        } else {
+            balance -= amount;
         }
     }
 
-    public void redChip(){
-        if (balance < 5){
-            System.out.println("You don't have enough cash!");
-        }
-        else{
-            balance -= 5;
-        }
+    public void whiteChip() {
+        deduct(1);
     }
 
-    public void greenChip(){
-        if (balance < 25){
-            System.out.println("You don't have enough cash!");
-        }
-        else{
-            balance -= 25;
-        }
+    public void redChip() {
+        deduct(5);
     }
 
-    public void blackChip(){
-        if (balance < 100){
-            System.out.println("You don't have enough cash!");
-        }
-        else{
-            balance -= 100;
-        }
+    public void greenChip() {
+        deduct(25);
     }
 
-    public void purpleChip(){
-        if (balance < 500){
-            System.out.println("You don't have enough cash!");
-        }
-        else{
-            balance -= 500;
-        }
+    public void blackChip() {
+        deduct(100);
     }
 
-    public void yellowChip(){
-        if (balance < 1000){
-            System.out.println("You don't have enough cash!");
-        }
-        else{
-            balance -= 1000;
-        }
+    public void purpleChip() {
+        deduct(500);
     }
 
+    public void yellowChip() {
+        deduct(1000);
+    }
 }
-
