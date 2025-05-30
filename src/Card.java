9d4b2ed8674527827b9efb4141014de75fcf51a6
LeoChen128 <leo1281515@gmail.com>
2025-05-29 23:55:03 -0400

    Updated the cards so that the deck is now being used properly and it can be distributed to other players.

--- src/Card.java
+++ src/Card.java
@@ -5,43 +5,40 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.awt.Rectangle;
 
-public class Card {
-    private String suit;
+private String suit;
     private String value;
     private String imageFileName;
-    private String backImageFileName;
     private boolean show;
     private BufferedImage image;
     private Rectangle cardBox;
-    private ArrayList<Card> deck;
-
 
     public Card(String suit, String value) {
         this.suit = suit;
         this.value = value;
-        this.imageFileName = "images/card_"+suit+"_"+value+".png";
+        this.imageFileName = "images/card_" + suit + "_" + value + ".png";
         this.show = true;
-        this.backImageFileName = "images/card_back.png";
-        this.image = readImage();
-        this.cardBox = new Rectangle(-100, -100, image.getWidth(), image.getHeight());
-        this.deck = new ArrayList<Card>();
+        this.cardBox = new Rectangle(-100, -100, 71, 96);
+        loadDefaultImage();
     }
 
-    public Rectangle getCardBox() {
-        return cardBox;
+    public void loadDefaultImage() {
+        image = new BufferedImage(71, 96, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2d = image.createGraphics();
+        g2d.setColor(Color.WHITE);
+        g2d.fillRect(0, 0, 71, 96);
+        g2d.setColor(Color.BLACK);
+        g2d.drawRect(0, 0, 70, 95);
+        g2d.setFont(new Font("Arial", Font.BOLD, 12));
+        g2d.drawString(value, 5, 20);
+        g2d.drawString(suit, 5, 40);
+        g2d.dispose();
     }
 
-    public String getSuit() {
-        return suit;
-    }
-
-    public void setRectangleLocation(int x, int y) {
-        cardBox.setLocation(x, y);
-    }
-
-    public String getValue() {
-        return value;
-    }
+    public String getSuit() { return suit; }
+    public String getValue() { return value; }
+    public BufferedImage getImage() { return image; }
+    public Rectangle getCardBox() { return cardBox; }
+    public void setRectangleLocation(int x, int y) { cardBox.setLocation(x, y); }
 
     public int getNumericalValue() {
         if (value.equals("A")) return 14;
@@ -51,47 +48,16 @@ public class Card {
         return Integer.parseInt(value);
     }
 
-    public String getImageFileName() {
-        return imageFileName;
-    }
-
-    public String toString() {
-        return suit + " " + value;
-    }
-
-    public BufferedImage getImage()  {
-        return image;
-    }
-
-
-    public BufferedImage readImage() {
-        try {
-            BufferedImage image;
-            image = ImageIO.read(new File(imageFileName));
-            return image;
-        }
-        catch (IOException e) {
-            System.out.println(e);
-            return null;
-        }
-    }
-
-    public ArrayList<Card> buildDeck() {
+    public static ArrayList<Card> buildDeck() {
+        ArrayList<Card> deck = new ArrayList<>();
         String[] suits = {"clubs", "diamonds", "hearts", "spades"};
-        String[] values = {"02", "03", "04", "05", "06", "07", "08", "09", "10", "A", "J", "K", "Q"};
+        String[] values = {"02", "03", "04", "05", "06", "07", "08", "09", "10", "J", "Q", "K", "A"};
         for (String s : suits) {
             for (String v : values) {
-                Card c = new Card(s, v);
-                deck.add(c);
+                deck.add(new Card(s, v));
             }
         }
+        Collections.shuffle(deck);
         return deck;
     }
-    public Card draw() {
-        if (deck.size() > 0) {
-            return deck.remove(deck.size() - 1);
-        }
-        return null;
-    }
-
 }
